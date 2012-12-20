5fdbe455e0660e7da0bc4e5225c8b89d3e9451e4
Nathan Cahill <nathan@nathancahill.com>
2012-12-20 01:36:33 -0500

    updated for mountain lion without appscript

--- README.md
+++ README.md
@@ -2,13 +2,12 @@
 
 Solves the problem of remembering the abstract names of applications on your Mac.
 It scrapes a short description of the app from MacUpdate.com, and saves it to the app's Spotlight comments.
-Then, you can easily find the app with Spotlight by searching for it's function or keyword.
+Then, you can easily find the app with Spotlight by searching for keywords.
 
 ### Pre-requisites
 
-This script uses appscript and BeautifulSoup. The easiest way to install these is using pip:
+This script uses BeautifulSoup for parsing data from MacUpdate.com. The easiest way to install this is with pip:
 
-    pip install appscript
     pip install beautifulsoup4
 
 ### Using the script
--- app_keywords.py
+++ app_keywords.py
@@ -2,7 +2,7 @@
 # encoding: utf-8
 
 #	Add keywords and descriptions to your Mac applications
-#	
+#
 #	Solves the problem of remembering the abstract names of applications on your computer.
 #	It scrapes a short description of the app from MacUpdate.com, and saves it to the app's Spotlight comments.
 #	Then, you can easily find the app with Spotlight by searching for it's function or keyword.
@@ -27,36 +27,30 @@
 
 import os
 import glob
-import sys
 import urllib2
-
-from appscript import *
 from bs4 import BeautifulSoup
 
-applications = "/Applications/"
+applications = '/Applications/'
+macupdate_url = 'http://www.macupdate.com/find/mac/'
+script = """osascript -e 'tell application "Finder" to set comment of (POSIX file "/Applications/%s" as alias) to "%s"'"""
 
-def main():
-	# Loop through Applications folder
-	for currentFile in glob.glob( os.path.join(applications, '*.app') ):
-	    if os.path.isdir(currentFile):
-	        (name, ext) = os.path.basename(currentFile).split(".app")
-	        print("Searching for " + name + "...")
-	        # Search and scrape MacUpdate.com for the application
-	        html = urllib2.urlopen('http://www.macupdate.com/find/mac/' + urllib2.quote(name)).read()
-	        desc = BeautifulSoup(html).find_all("span", {'class': 'shortdes'})
+# Loop through Applications folder
+for currentFile in glob.glob(os.path.join(applications, '*.app')):
+    if os.path.isdir(currentFile):
+        (name, ext) = os.path.basename(currentFile).split('.app')
+        print('Searching for ' + name + '...')
 
-	        if not desc:
-	        	print("Could not find a match for " + name)
-	        	print(" ")
+        # Search and scrape MacUpdate.com for the application
+        html = urllib2.urlopen(macupdate_url + urllib2.quote(name)).read()
+        desc = BeautifulSoup(html).find_all('span', {'class': 'shortdes'})
 
-	        else:
-	        	desc = desc[0].get_text().lstrip(' - ')
-	        	print("Found this: " + desc)
-	        	# Add description to Spotlight Comments (to view, select the application and Command+I)
-	        	file = app("Finder").items[mactypes.Alias(applications + os.path.basename(currentFile))].comment.set(desc)
-	        	print(" ")
+        if not desc:
+            print('Could not find a match for ' + name)
+            print(' ')
 
-if __name__ == '__main__':
-    main()
+        else:
+            desc = desc[0].get_text().lstrip(' - ')
 
-
+            # Add description to Spotlight Comments
+            os.system(script % (os.path.basename(currentFile), desc))
+            print(' ')
